e81d265c100b705423d220a99426262054cbe45e
Caldm <1832400547@qq.com>
2026-05-08 15:51:42 +0800

    feat(SearchDetail.vue): 添加路由参数处理方法

--- src/views/searchDetail/SearchDetail.vue
+++ src/views/searchDetail/SearchDetail.vue
@@ -1,5 +1,28 @@
 <script setup lang="ts">
+import {useLoadingStore} from "@/store/useLoadingStore.ts";
+import {onMounted} from "vue";
+import {useRoute} from "vue-router";
 
+const route = useRoute();
+const loadingStore = useLoadingStore();
+
+const fetchSearchResult = async (params: object) => {
+  console.log(JSON.stringify(params, null, 2));
+}
+
+const handleRouteQueryParams = async () => {
+  const _ = {...route.query};
+  if (!('src' in _)) _.src = 'unknown';
+  if (!('sort' in _)) _.sort = 'created_at';
+  if (!('timestamp' in _)) _.sort = new Date().getTime().toString();
+  if (!('q' in _)) _.q = '';
+  await fetchSearchResult(_);
+}
+
+onMounted(async () => {
+  await handleRouteQueryParams();
+  loadingStore.endLoading();
+})
 </script>
 
 <template>
