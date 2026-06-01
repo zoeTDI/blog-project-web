5046d39424fbf818a4e28077c8d0d22e8f295831
Caldm <1832400547@qq.com>
2026-06-01 23:12:40 +0800

    fix(guard.ts): 修复访问登录页面时，标签页标题未处理的问题

--- src/router/guard.ts
+++ src/router/guard.ts
@@ -3,6 +3,8 @@ import {useLoadingStore} from "@/store/useLoadingStore.ts";
 import {ROUTER_NAMES} from "@/router/routerNames.ts";
 import {useUserStore} from "@/store/useUserStore.ts";
 
+const userStore = useUserStore()
+
 const handleDocumentTitle = (to: RouteLocationNormalized) => {
     // 修改标题
     let title = '';
@@ -18,6 +20,30 @@ const handleDocumentTitle = (to: RouteLocationNormalized) => {
     document.title = title ? `${title} - ${siteName}` : siteName;
 }
 
+const handleLogin = (to: RouteLocationNormalized) => {
+    const authToken = userStore.getAuthToken()
+    if (authToken && authToken !== '') {
+        // 用户成功登录并且url中包含目的地址
+        if (to.query?.to && to.query.to !== '') {
+            return {path: to.query.to};
+        }
+        // 用户在已登录情况下手动访问登录页
+        if (to.name == ROUTER_NAMES.LOGIN) {
+            return {name: ROUTER_NAMES.NOT_FOUND};
+        }
+        // 放行
+        return true;
+    } else {
+        // 没有token，跳转登录页
+        return {
+            name: ROUTER_NAMES.LOGIN,
+            query: {
+                to: to.path
+            }
+        }
+    }
+}
+
 /**
  * 路由守卫函数
  * @param router 路由实例
@@ -25,43 +51,21 @@ const handleDocumentTitle = (to: RouteLocationNormalized) => {
 export function setupRouterGuard(router: Router) {
     // 前置守卫
     router.beforeEach(async (to, from) => {
+        let toRouter = {name: ROUTER_NAMES.HOME};
         // 设置加载进度条
         const loadingStore = useLoadingStore();
 
         loadingStore.startLoading();
 
-        if (to.name === ROUTER_NAMES.TAG_DETAIL && !to.query?.id) {
-            return {name: ROUTER_NAMES.NOT_FOUND};
-        }
-
         if (to.meta?.requireLogin) {
             // 需要登录
-            const userStore = useUserStore()
-            const authToken = userStore.getAuthToken()
-            if (authToken && authToken !== '') {
-                // 用户在已登录情况下手动访问登录页
-                if (to.name == ROUTER_NAMES.LOGIN) {
-                    return {name: ROUTER_NAMES.NOT_FOUND};
-                }
-                // 用户成功登录并且url中包含目的地址
-                if (to.query?.to && to.query.to !== '') {
-                    return {path: to.query.to};
-                }
-                // 放行
-                return true;
-            } else {
-                // 没有token，跳转登录页
-                return {
-                    name: ROUTER_NAMES.LOGIN,
-                    query: {
-                        to: to.path
-                    }
-                }
-            }
+            toRouter = handleLogin(to)
+        } else {
+            toRouter = true;
         }
 
         handleDocumentTitle(to);
-        return true;
+        return toRouter;
     });
 
     // 后置守卫
