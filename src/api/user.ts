ce4f4e94094b5afa91619243708777d14999826a
Caldm <1832400547@qq.com>
2026-06-01 15:14:27 +0800

    feat: 添加获取后台路由的api方法

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,10 +1,16 @@
 import {defHttp} from "@/utils/request.ts";
 import type {UserInfo} from "#/user.ts";
+import type {MenuDataItem} from "#/router";
 
 enum Api {
-    user = "/user"
+    user = "/user",
+    userMenu = '/user/menu'
 }
 
 export const getUserInfo = (id: number) => {
     return defHttp.get<UserInfo>(Api.user, {id});
+}
+
+export const getUserMenu = (id: string) => {
+    return defHttp.post<MenuDataItem[]>(Api.userMenu, {id})
 }
