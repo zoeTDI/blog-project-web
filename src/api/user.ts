511e29e08eac94a7ed3c0493c7c492fe1c3c0d37
Caldm <1832400547@qq.com>
2026-06-01 22:21:01 +0800

    revert(router): 移除动态路由

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,16 +1,10 @@
 import {defHttp} from "@/utils/request.ts";
 import type {UserInfo} from "#/user.ts";
-import type {MenuDataItem} from "#/router";
 
 enum Api {
     user = "/user",
-    userMenu = '/user/menu'
 }
 
 export const getUserInfo = (id: number) => {
     return defHttp.get<UserInfo>(Api.user, {id});
-}
-
-export const getUserMenu = (id: string) => {
-    return defHttp.post<MenuDataItem[]>(Api.userMenu, {id})
 }
