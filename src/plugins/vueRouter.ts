872f770c081e8ae9c621c31134232cc0106c9008
Caldm <1832400547@qq.com>
2026-06-04 00:54:44 +0800

    feat(router): 根据环境文件配置router的history类型

--- src/plugins/vueRouter.ts
+++ src/plugins/vueRouter.ts
@@ -1,12 +1,20 @@
 import type {App} from "vue";
-import {createRouter, createWebHashHistory} from "vue-router";
+import {createRouter, createWebHashHistory, createWebHistory} from "vue-router";
 import routes from '../router/index.ts'
 import {setupRouterGuard} from "../router/guard.ts";
 
 const router = createRouter({
-    history: createWebHashHistory(),
+    history:
+        import.meta.env.VITE_ROUTER_HISTORY === 'hash'
+            ? createWebHashHistory()
+            : createWebHistory(),
     routes,
-    scrollBehavior: () => ({left: 0, right: 0})
+    scrollBehavior: (to, _form, savedPosition) => {
+        if (savedPosition) {
+            return savedPosition;
+        }
+        return to.hash ? {behavior : 'smooth', el: to.hash} : {left: 0, right: 0}
+    }
 })
 
 export function setupRouter(app: App<Element>) {
