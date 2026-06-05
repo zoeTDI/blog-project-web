4d587aa3cd46fa22daad097e713d64f90fd18263
Caldm <1832400547@qq.com>
2026-06-05 14:52:04 +0800

    refactor(cache): 简化cache桶文件

--- src/cache/index.ts
+++ src/cache/index.ts
@@ -1,17 +1,4 @@
-import {StorageManager} from "@/cache/src/StorageManager.ts";
-import type {StorageManagerOptions, IStorageDriver, IStorageItem, StorageType, LocalStorageDriverOptions} from '@/cache/src/type.ts'
-import {LocalStorageDriver} from "@/cache/src/LocalStorageDriver.ts";
-import {MemoryStorageDriver} from "@/cache/src/MemoryStorageDriver.ts";
-
-
-export {
-  StorageManager,
-  LocalStorageDriver,
-  MemoryStorageDriver,
-  
-  StorageManagerOptions,
-  IStorageDriver,
-  IStorageItem,
-  StorageType,
-  LocalStorageDriverOptions
-}
+export * from '@/cache/src/type.ts'
+export * from '@/cache/src/LocalStorageDriver.ts'
+export * from '@/cache/src/MemoryStorageDriver.ts'
+export * from '@/cache/src/StorageManager.ts'
