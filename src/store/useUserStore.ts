d2e960187b236d0c945414fe34413864cb6901fd
Caldm <1832400547@qq.com>
2026-06-08 13:41:00 +0800

    feat(useUserStore.ts): 格式化代码，添加logout方法

--- src/store/useUserStore.ts
+++ src/store/useUserStore.ts
@@ -2,45 +2,52 @@ import {defineStore} from "pinia";
 import {ref} from "vue";
 
 export const useUserStore = defineStore(
-    'user',
-    () => {
-        const authToken = ref<string>('')
-        const role = ref<string>('');
-        const menus = ref<string[]>([])
-
-        const getAuthToken = () => {
-            return authToken.value;
-        }
-        const setAuthToken = (value: string) => {
-            authToken.value = value;
-        }
-        const getRole = () => {
-            return role.value;
-        }
-        const setRole = (value: string) => {
-            role.value = value;
-        }
-        const getMenus = () => {
-            return menus.value;
-        }
-        const setMenus = (value: string[]) => {
-            menus.value = value
-        }
-        return {
-            authToken,
-            role,
-            menus,
-            getAuthToken,
-            setAuthToken,
-            getRole,
-            setRole,
-            getMenus,
-            setMenus
-        }
-    },
-    {
-        persist: {
-            pick: ['authToken', 'role', 'menus']
-        }
+  'user',
+  () => {
+    const authToken = ref<string>('')
+    const role = ref<string>('');
+    const menus = ref<string[]>([])
+    
+    const getAuthToken = () => {
+      return authToken.value;
     }
+    const setAuthToken = (value: string) => {
+      authToken.value = value;
+    }
+    const getRole = () => {
+      return role.value;
+    }
+    const setRole = (value: string) => {
+      role.value = value;
+    }
+    const getMenus = () => {
+      return menus.value;
+    }
+    const setMenus = (value: string[]) => {
+      menus.value = value
+    }
+    
+    const logout = () => {
+      authToken.value = '';
+      role.value = '';
+      menus.value = []
+    }
+    return {
+      authToken,
+      role,
+      menus,
+      getAuthToken,
+      setAuthToken,
+      getRole,
+      setRole,
+      getMenus,
+      setMenus,
+      logout
+    }
+  },
+  {
+    persist: {
+      pick: ['authToken', 'role', 'menus']
+    }
+  }
 )
