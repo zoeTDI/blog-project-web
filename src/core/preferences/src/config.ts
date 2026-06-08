ca4de14d1de757b3526308f211bd5e377662383d
Caldm <1832400547@qq.com>
2026-06-08 15:50:16 +0800

    refactor(config.ts): 修改用户的默认首页

--- src/core/preferences/src/config.ts
+++ src/core/preferences/src/config.ts
@@ -3,7 +3,7 @@ import {type Preferences, TimezoneOptions} from "@/core/preferences";
 const defaultPreferences: Preferences = {
   app: {
     defaultAvatar: "",
-    defaultHomePath: "/home",
+    defaultHomePath: "/dashboard",
     locale: 'zh-CN',
     timezone: TimezoneOptions.UTC,
     watermark: true,
