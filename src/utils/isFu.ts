a7eee31be596047666994fab9abf5f89b825c65b
Caldm <1832400547@qq.com>
2026-06-08 10:36:52 +0800

    feat(utils): 添加isArray函数用于判断是否为数组

--- src/utils/isFu.ts
+++ src/utils/isFu.ts
@@ -1,3 +1,7 @@
 export const isObject = (item: unknown): item is Record<string, any> => {
   return (item && typeof item === 'object' && !Array.isArray(item));
 }
+
+export const isArray = (item: unknown): item is Array => {
+  return Array.isArray(item);
+}
