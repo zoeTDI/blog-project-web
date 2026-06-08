7a25a67b79940a7ecb8f0e3ada190d34bd52085e
Caldm <1832400547@qq.com>
2026-06-08 12:57:48 +0800

    feat(core/preferences): 创建core/preferences的桶文件
    分为普通路由守卫和权限路由守卫

---
+++ src/core/preferences/index.ts
@@ -0,0 +1,4 @@
+export * from '@/core/preferences/src/type.ts'
+export * from '@/core/preferences/src/preference.ts'
+export * from '@/core/preferences/src/config.ts'
+export * from '@/core/preferences/src/updateCssVariables.ts'
