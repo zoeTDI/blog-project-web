6856595e04a8990daf44164c54ea0d5866163060
Caldm <1832400547@qq.com>
2026-06-11 17:00:37 +0800

    fix(analysis.ts): 修复ts类型错误

--- src/router/modules/analysis.ts
+++ src/router/modules/analysis.ts
@@ -1,34 +1,32 @@
-import type {RouteRecordRaw} from "vue-router";
-import {ChartBarSquareIcon, Squares2X2Icon} from '@heroicons/vue/24/outline'
-
+import type { RouteRecordRaw } from 'vue-router';
+import { ChartBarSquareIcon, Squares2X2Icon } from '@heroicons/vue/24/outline';
 
 export const BACKEND_ROUTER_NAME = {
-    DASHBOARD: 'Dashboard',
-}
+  DASHBOARD: 'Dashboard',
+};
 
-const analysisRouter: RouteRecordRaw = [
-    {
+const analysisRouter: RouteRecordRaw[] = [
+  {
+    meta: {
+      title: `分析`,
+      icon: Squares2X2Icon,
+    },
+    path: '/analysis',
+    component: () => import('@/layouts/BackendLayout.vue'),
+    redirect: '/analysis/dashboard',
+    children: [
+      {
+        path: '/analysis/dashboard',
+        name: BACKEND_ROUTER_NAME.DASHBOARD,
+        component: () => import('@/views/analysis/dashboard/Dashboard.vue'),
         meta: {
-            title: `分析`,
-            icon: Squares2X2Icon
+          title: '仪表盘',
+          requireLogin: true,
+          icon: ChartBarSquareIcon,
         },
-        path: '/analysis',
-        component: () => import('@/layouts/BackendLayout.vue'),
-        redirect: '/analysis/dashboard',
-        children: [
-            {
-                path: '/analysis/dashboard',
-                name: BACKEND_ROUTER_NAME.DASHBOARD,
-                component: () => import('@/views/analysis/dashboard/Dashboard.vue'),
-                meta: {
-                    title: '仪表盘',
-                    requireLogin: true,
-                    icon: ChartBarSquareIcon
-                }
-            }
-        ]
-    }
-    
-]
+      },
+    ],
+  },
+];
 
-export default analysisRouter
+export default analysisRouter;
