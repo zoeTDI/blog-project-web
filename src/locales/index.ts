36d4893e0d9fa58a200c77b6b4ca70546cc0a93c
Caldm <1832400547@qq.com>
2026-06-11 23:49:02 +0800

    style(locales): 移除调试语句

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -1,6 +1,5 @@
 // 自动导入语言json文件
 const modules = import.meta.glob('./**/*.json', { eager: true });
-console.log('🚀 ~  ~ modules: ', modules);
 const messages: Record<string, any> = {};
 
 for (const path in modules) {
