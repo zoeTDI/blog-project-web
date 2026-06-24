c7be07289a8237fcf5796f64aee229f89349835a
Caldm <1832400547@qq.com>
2026-06-24 12:51:21 +0800

    perf(i18n): 移除未使用的函数

--- src/plugins/i18n.ts
+++ src/plugins/i18n.ts
@@ -1,4 +1,4 @@
-import { createI18n, useI18n } from 'vue-i18n';
+import { createI18n } from 'vue-i18n';
 import { messages } from '@/locales';
 import type { App } from 'vue';
 import {
@@ -14,18 +14,6 @@ const getInitialLocale = () => {
   return preferences.app.locale || SUPPORT_LANGUAGE_OPTIONS.zh_CN;
 };
 
-/**
- * 获取翻译后的字符串，如果没有，则返回传入的字符串
- * @param str
- */
-const translateString = (str: string): string => {
-  let translation = str;
-  if (translation.startsWith(ROUTER_PREFIX)) {
-    translation = useI18n().t(translation);
-  }
-  return translation;
-};
-
 const i18n = createI18n({
   locale: getInitialLocale(),
   fallbackLocale: SUPPORT_LANGUAGE_OPTIONS.en_US,
@@ -38,4 +26,4 @@ const setupI18n = (app: App<Element>) => {
 
 const $t = i18n.global.t;
 
-export { setupI18n, i18n, $t, ROUTER_PREFIX, DEFAULT_MESSAGE, translateString };
+export { setupI18n, i18n, $t, ROUTER_PREFIX, DEFAULT_MESSAGE };
