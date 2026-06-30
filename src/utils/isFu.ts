98945128d5be9bfd0c22e87561713e3c4398e85c
Caldm <1832400547@qq.com>
2026-06-30 13:57:27 +0800

    feat(utils): 添加日期判断函数

--- src/utils/isFu.ts
+++ src/utils/isFu.ts
@@ -6,7 +6,15 @@ const isArray = (item: any): boolean => {
   return Array.isArray(item);
 };
 
+const isSameDay = (d1: Date, d2: Date) => {
+  return (
+    d1.getFullYear() === d2.getFullYear() &&
+    d1.getMonth() === d2.getMonth() &&
+    d1.getDate() === d2.getDate()
+  );
+};
+
 const isDev = (): boolean => import.meta.env.DEV;
 const isProd = (): boolean => import.meta.env.PROD;
 
-export { isObject, isArray, isDev, isProd };
+export { isObject, isArray, isDev, isProd, isSameDay };
