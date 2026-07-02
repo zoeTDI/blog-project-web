10a4f3a985ef135cb074c2f9a05867ecf5ed5cfd
Caldm <1832400547@qq.com>
2026-07-02 13:48:53 +0800

    refactor(style):
    - 将css样式绑定到root上
    - 主题和主题色的变更方法也修改为对html元素的操作

--- src/core/preferences/src/updateCssVariables.ts
+++ src/core/preferences/src/updateCssVariables.ts
@@ -8,16 +8,18 @@ const updateThemeMode = (themePreferences: DeepPartial<ThemePreferences>) => {
   if (Object.keys(themePreferences).includes('mode')) {
     const themeModeOption: ThemeModeOptions | undefined = themePreferences.mode;
     if (themeModeOption === undefined) return;
-    const app = document.getElementById('app');
-    if (!app) {
+    // const app = document.getElementById('app');
+    const els = document.getElementsByTagName('html');
+    if (els.length == 0) {
       console.error(`无法获取到#app元素`);
       return;
     }
-    if (app.classList.contains(themeModeOption)) {
+    const el = els[0];
+    if (el.classList.contains(themeModeOption)) {
       return;
     }
-    app.classList.add(themeModeOption);
-    app.classList.remove(themeModeOption === 'dark' ? 'light' : 'dark');
+    el.classList.add(themeModeOption);
+    el.classList.remove(themeModeOption === 'dark' ? 'light' : 'dark');
     if (themePreferences.colorPrimary) {
       updateThemeColor(themePreferences.colorPrimary);
     }
@@ -25,9 +27,11 @@ const updateThemeMode = (themePreferences: DeepPartial<ThemePreferences>) => {
 };
 
 export const updateThemeColor = (color: string) => {
-  const appEl = document.getElementById('app');
-  if (appEl) {
-    appEl.style.setProperty('--app-accent-color', color);
+  // const el = document.getElementById('app');
+  const els = document.getElementsByTagName('html');
+  if (els.length > 0) {
+    const el = els[0];
+    el.style.setProperty('--app-accent-color', color);
   }
 };
 
