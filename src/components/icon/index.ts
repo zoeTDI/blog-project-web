0514c27fe01a63e642c041cde4a1f42a6a8b3b2e
Caldm <1832400547@qq.com>
2026-07-07 20:36:12 +0800

    feat(HeroIcon): 封装了HeroIcon图标

--- src/components/icon/index.ts
+++ src/components/icon/index.ts
@@ -1,7 +1,10 @@
 import SimpleIcon from '@/components/icon/src/simpleIcon/src/SimpleIcon.vue';
+import HeroIcon from '@/components/icon/src/heroIcon/src/HeroIcon.vue';
 
 export {
-  SimpleIcon
+  SimpleIcon,
+  HeroIcon
 }
 
 export type * from './src/simpleIcon/src/types.ts'
+export type * from './src/heroIcon/src/types.ts'
