f997b671c0a14ca7936aa559cbefbcd3eedcb0bb
Caldm <1832400547@qq.com>
2026-07-10 01:16:36 +0800

    project(storybook): 在storybook中引入全局样式

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,12 +1,12 @@
-import type { Preview } from '@storybook/vue3-vite'
-import '@/style.css'
+import type { Preview } from '@storybook/vue3-vite';
+import '@/style.css';
 
 const preview: Preview = {
   parameters: {
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
     },
   },
