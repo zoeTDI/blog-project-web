72eb3ecb8e2a8e0ada0468ad3cb6fec559f3f531
Caldm <1832400547@qq.com>
2026-07-11 13:52:58 +0800

    feat(utils): 添加更多判断函数

--- src/utils/isFu.ts
+++ src/utils/isFu.ts
@@ -17,4 +17,42 @@ const isSameDay = (d1: Date, d2: Date) => {
 const isDev = (): boolean => import.meta.env.DEV;
 const isProd = (): boolean => import.meta.env.PROD;
 
-export { isObject, isArray, isDev, isProd, isSameDay };
+const isString = (val: any): val is string => typeof val === 'string';
+const isUndefined = (val: any): val is undefined => val === undefined;
+const isBoolean = (val: any): val is boolean => typeof val === 'boolean';
+const isNumber = (val: any): val is number => typeof val === 'number';
+
+const isEmpty = (val: unknown) =>
+  (!val && val !== 0) ||
+  (isArray(val) && (val as any[]).length === 0) ||
+  (isObject(val) && !Object.keys(val).length);
+
+const isElement = (e: unknown): e is Element => {
+  if (typeof Element === 'undefined') return false;
+  return e instanceof Element;
+};
+
+const isStringNumber = (val: string): boolean => {
+  if (!isString(val)) {
+    return false;
+  }
+  return !Number.isNaN(Number(val));
+};
+
+const isWindow = (val: unknown): val is Window => val === window;
+
+export {
+  isObject,
+  isArray,
+  isDev,
+  isProd,
+  isSameDay,
+  isString,
+  isUndefined,
+  isBoolean,
+  isNumber,
+  isEmpty,
+  isElement,
+  isStringNumber,
+  isWindow,
+};
