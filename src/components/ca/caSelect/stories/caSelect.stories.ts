153f82bfecef048f62eb378743846b073e7b723a
Caldm <1832400547@qq.com>
2026-07-15 14:21:24 +0800

    feat: 添加演示文件

--- src/components/ca/caSelect/stories/caSelect.stories.ts
+++ src/components/ca/caSelect/stories/caSelect.stories.ts
@@ -4,6 +4,7 @@ import {
   CaSelectGroup,
 } from '@/components/ca/caSelect';
 import type { Meta, StoryObj } from '@storybook/vue3-vite';
+import { ref } from 'vue';
 
 const meta = {
   title: 'Component/Ca/CaSelect',
@@ -14,8 +15,19 @@ const meta = {
     placeholder: {
       control: 'text',
     },
+    size: {
+      control: 'select',
+      options: ['S', 'M', 'L'],
+    },
+    disabled: {
+      control: 'boolean',
+    },
   },
-  args: {},
+  args: {
+    placeholder: 'Select',
+    size: 'M',
+    disabled: false,
+  },
 } satisfies Meta<typeof CaSelect>;
 
 export default meta;
@@ -30,34 +42,116 @@ export const Default: Story = {
     },
     template: `
       <CaSelect v-bind="args">
-        <CaSelectOption v-for="i in 10" :key="i">{{ i }} value</CaSelectOption>
+        <CaSelectOption v-for="i in 10" :key="i" :value="i" :label="'value ' + i" />
       </CaSelect>
     `,
   }),
 };
 
-export const Group: Story = {
-  args: {
-    options: [
-      {
-        label: 'Group 1',
-        options: ['Value 1', 'Value 2', 'Value 3'],
-      },
-      {
-        label: 'Group 2',
-        options: ['Value 1', 'Value 2', 'Value 3'],
-      },
-    ],
-  },
+export const DefaultValue: Story = {
+  args: {},
   render: (args) => ({
-    components: { CaSelect, CaSelectGroup, CaSelectItem: CaSelectOption },
+    components: { CaSelect, CaSelectOption },
     setup() {
-      return { args };
+      const selectedValue = ref(2);
+      return { args, selectedValue };
+    },
+    template: `
+      <CaSelect v-bind="args" v-model="selectedValue">
+        <CaSelectOption v-for="i in 10" :key="i" :value="i" :label="'value ' + i" />
+      </CaSelect>
+    `,
+  }),
+};
+
+export const Size: Story = {
+  args: {},
+  render: (args) => ({
+    components: { CaSelect, CaSelectOption },
+    setup() {
+      return { args, size: ['S', 'M', 'L'] };
+    },
+    template: `
+      <div style="display: flex; gap: 20px;">
+        <CaSelect v-for="item in size" :key="item" :size="item">
+          <CaSelectOption v-for="i in 10" :key="i" :value="i" :label="'value ' + i" />
+        </CaSelect>
+      </div>
+    `,
+  }),
+};
+
+export const Group: Story = {
+  args: {},
+  render: (args) => ({
+    components: { CaSelect, CaSelectGroup, CaSelectOption },
+    setup() {
+      const data = [
+        {
+          label: 'Group 1',
+          options: [
+            { label: 'Value 1', value: 'Value 1' },
+            { label: 'Value 2', value: 'Value 2' },
+            { label: 'Value 3', value: 'Value 3' },
+          ],
+        },
+        {
+          label: 'Group 2',
+          options: [
+            { label: 'Value 1', value: 'Value 1' },
+            { label: 'Value 2', value: 'Value 2' },
+            { label: 'Value 3', value: 'Value 3' },
+          ],
+        },
+      ];
+      return { args, data };
     },
     template: `
       <CaSelect>
-        <CaSelectGroup v-for="group in args.options" :key="group.label" :label="group.label">
-          <CaSelectOption v-for="option in group.options" :key="option">{{ option }}</CaSelectOption>
+        <CaSelectGroup v-for="group in data" :key="group.label" :label="group.label">
+          <CaSelectOption v-for="option in group.options" :key="option.value" :value="option.value"
+                          :label="option.label" />
+        </CaSelectGroup>
+      </CaSelect>
+    `,
+  }),
+};
+
+export const LongLabel: Story = {
+  args: {},
+  render: (args) => ({
+    components: { CaSelect, CaSelectGroup, CaSelectOption },
+    setup() {
+      const data = [
+        {
+          label:
+            'Group 1 Long Text Long Text Long Text Long Text Long Text Long Text Long Text Long Text',
+          options: [
+            {
+              label:
+                'Value 1 Long Text Long Text Long Text Long Text Long Text Long Text Long Text Long Text',
+              value: 'Value 1',
+            },
+            { label: 'Value 2', value: 'Value 2' },
+            { label: 'Value 3', value: 'Value 3' },
+          ],
+        },
+        {
+          label: 'Group 2',
+          options: [
+            { label: 'Value 4', value: 'Value 4' },
+            { label: 'Value 5', value: 'Value 5' },
+            { label: 'Value 6', value: 'Value 6' },
+          ],
+        },
+      ];
+      return { args, data };
+    },
+    template: `
+      <CaSelect>
+        <CaSelectGroup v-for="group in data" :key="group.label" :label="group.label">
+          <CaSelectOption v-for="option in group.options" :key="option.value" :value="option.value"
+                          :label="option.label" />
         </CaSelectGroup>
       </CaSelect>
     `,
