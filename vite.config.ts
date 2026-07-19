c07d3e509268436e4be108094e00452b752a4a09
Caldm <1832400547@qq.com>
2026-07-19 21:57:38 +0800

    feat: 设置项目端口

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import {resolve} from "path";
+import { fileURLToPath } from 'node:url';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': resolve(__dirname, './src'),
-      '#': resolve(__dirname, './src/types'),
-    }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '#': fileURLToPath(new URL('./src/types', import.meta.url)),
+    },
+  },
+  server: {
+    port: 13030,
+    strictPort: true,
+    host: true,
   }
-})
+});
