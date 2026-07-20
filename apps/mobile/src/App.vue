244ae89e48565c9685dfb00a764628e170aa6ae8
Caldm <1832400547@qq.com>
2026-07-20 10:24:08 +0800

    refactor: 清理vue代码

--- apps/mobile/src/App.vue
+++ apps/mobile/src/App.vue
@@ -1,12 +1,10 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
-import {SButton} from '@caldm/ui'
 </script>
 
 <template>
   <header>
-    <SButton />
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
