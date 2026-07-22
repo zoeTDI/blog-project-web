ec1630aff5c5f9685760450e96d07a6460d4d8a4
Caldm <1832400547@qq.com>
2026-07-22 18:00:17 +0800

    refactor(utils): 添加ts类型工具

---
+++ packages/utils/utils.d.ts
@@ -0,0 +1,26 @@
+/**
+ * 自定义类型函数：从常量对象中自动提取所有的 Value 类型
+ * T: 传入的常量对象类型
+ */
+export type ValueOf<T> = T[keyof T];
+
+/**
+ * 自定义类型函数：从对象类型中自动提取所有的 Key 类型
+ * T: 传入的对象类型
+ */
+export type KeyOf<T> = keyof T;
+
+/**
+ * 深度递归可选类型
+ * T: 传入的对象类型
+ */
+export type DeepPartial<T> = {
+  [P in keyof T]?: T[P] extends object ? DeepPartial<T[P]> : T[P];
+};
+
+/**
+ * 强制非空类型
+ * 从类型 T 中剔除掉 null 和 undefined
+ * 如果 T 本身就是 null 或 undefined，则返回 never
+ */
+export type NonNullable<T> = T extends null | undefined ? never : T;
