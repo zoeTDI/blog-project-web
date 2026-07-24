de7cd0eae3730238fdcd19742a5df680d9ffef67
Caldm <1832400547@qq.com>
2026-07-24 11:13:38 +0800

    refactor(web): 调整组件样式

--- apps/web/src/components/markdownRender/index.ts
+++ apps/web/src/components/markdownRender/index.ts
@@ -1,3 +1,6 @@
 import MarkdownRender from "@/components/markdownRender/src/MarkdownRender.vue";
 
+import '@/assets/markdown/css/vars.css'
+import '@/assets/markdown/css/base.css';
+
 export {MarkdownRender}
