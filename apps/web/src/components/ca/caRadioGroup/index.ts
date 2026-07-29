d86ded1091486a8eabf318b412fc6e5bf9d21174
Caldm <1832400547@qq.com>
2026-07-29 12:12:56 +0800

    refactor(web): 移除CaRadio组件

--- apps/web/src/components/ca/caRadioGroup/index.ts
+++
@@ -1,6 +0,0 @@
-import CaRadioGroup from './src/CaRadioGroup.vue';
-import CaRadio from './src/CaRadio.vue';
-import { RADIO_GROUP_KEY } from '@/components/ca/caRadioGroup/src/types.ts';
-
-export { CaRadioGroup, CaRadio, RADIO_GROUP_KEY };
-export type * from './src/types';
