82f972b61a7da370d3cb9fafdc07af44f71825d4
Caldm <1832400547@qq.com>
2026-07-29 14:22:19 +0800

    refactor(ui): 调整CaSwitchOption.value的类型

--- packages/ui/ca/switch/src/types.ts
+++ packages/ui/ca/switch/src/types.ts
@@ -3,14 +3,14 @@ import type { ComponentSize } from '#/component.ts';
 import type { ValueOf } from '@caldm/utils';
 import type { CaSwitchMode } from './constants.ts';
 
-export interface CaSwitchOption {
-  value: string | number | symbol;
+export interface CaSwitchOption<T = string> {
+  value: T;
   label: string;
   icon?: Component;
 }
 
-export interface CaSwitchProps {
-  options?: CaSwitchOption[];
+export interface CaSwitchProps<T = string> {
+  options?: CaSwitchOption<T>[];
   prefix?: string;
   mode?: ValueOf<typeof CaSwitchMode>;
   size?: ComponentSize;
