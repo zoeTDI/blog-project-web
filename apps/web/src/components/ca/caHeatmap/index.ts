39ed15d0e692644a9b76864b5251f937375256e6
Caldm <1832400547@qq.com>
2026-08-02 12:33:05 +0800

    refactor(web): 移除CaHeatmap组件

--- apps/web/src/components/ca/caHeatmap/index.ts
+++
@@ -1,5 +0,0 @@
-import CaHeatmap from '@/components/ca/caHeatmap/src/CaHeatmap.vue';
-import CaHeatmapTip from '@/components/ca/caHeatmap/src/CaHeatmapTip.vue';
-
-export { CaHeatmap, CaHeatmapTip };
-export type * from './src/types.ts';
