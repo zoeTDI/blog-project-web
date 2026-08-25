deb5226ebbbe13416b815843428eca320cc4d195
Caldm <1832400547@qq.com>
2026-08-26 01:54:09 +0800

    refactor(web): 实现token无感刷新

--- apps/web/src/utils/request.ts
+++ apps/web/src/utils/request.ts
@@ -1,16 +1,37 @@
-import type { Router } from 'vue-router';
 import { createHttp } from '@caldm/request';
+import { useUserStore } from '@/store/useUserStore';
+import { refreshToken } from '@/api/authApi';
+import { CaMessage } from '@caldm/ui';
+import router from '@/plugins/vueRouter'
+import { ROUTER_NAMES } from '@/router/routerNames.ts';
 
-const handleUnauthorized = async () => {
-  const router = (await import('@/router')).default as unknown as Router;
-  router.push({ name: 'Login' });
+const toLogin = async () => {
+  const store = useUserStore();
+  store.logout();
+  await router.push({ name: ROUTER_NAMES.LOGIN });
+};
+
+/**
+ * 处理身份验证失败
+ */
+const handleUnauthorized = async (): Promise<void> => {
+  await refreshToken();
+};
+
+const handleErrorMessage = async (errMsg: string) => {
+  CaMessage.error(errMsg);
+  await toLogin();
+};
+
+const handleRefreshFailed = async () => {
+  await toLogin();
+  CaMessage.error('登录失效，请重新登录。');
 };
 
 export const defHttp = createHttp({
   baseURL: '/api',
   timeout: 10000,
-  onErrorMessage: (msg) => {
-    console.error(msg);
-  },
   onUnauthorized: handleUnauthorized,
+  onErrorMessage: handleErrorMessage,
+  onRefreshFailed: handleRefreshFailed,
 });
--- packages/request/src/index.ts
+++ packages/request/src/index.ts
@@ -12,9 +12,17 @@ export interface HttpConfig {
   baseURL: string;
   timeout?: number;
   getToken?: () => string | null | Promise<string | null>;
-  onUnauthorized?: () => void;
-  onErrorMessage?: (msg: string) => void;
+  onUnauthorized?: () => Promise<void>;
+  onRefreshFailed?: () => Promise<void>;
+  onErrorMessage?: (msg: string) => Promise<void>;
 }
+
+let isRefreshing = false;
+let failedRequestsQueue: Array<{
+  resolve: (value: any) => void;
+  reject: (reason?: any) => void;
+  config: InternalAxiosRequestConfig;
+}> = [];
 
 export function createHttp(options: HttpConfig) {
   const service = axios.create({
@@ -36,7 +44,6 @@ export function createHttp(options: HttpConfig) {
     (response: AxiosResponse) => {
 
       const res = response.data as ResponseData;
-
       if (res.code === 200) {
         return res.data;
       }
@@ -51,10 +58,43 @@ export function createHttp(options: HttpConfig) {
 
       return Promise.reject(new Error(errorMsg));
     },
-    (error) => {
+    async (error) => {
       if (error.response) {
+        const originalRequest = error.config;
+
+        // 身份验证失败，尝试刷新token
         if (error.response.status === 401) {
-          options.onUnauthorized?.();
+          // 自定义特殊状态码，表示刷新token失败
+          if (error.response.data.code === 486) {
+            isRefreshing = false;
+            failedRequestsQueue = [];
+            await options.onRefreshFailed?.();
+            return Promise.reject(error);
+          }
+          // 已经在刷新token中了，将请求加入队列，等待重发。
+          if (isRefreshing) {
+            return new Promise((resolve, reject) => {
+              failedRequestsQueue.push({ resolve, reject, config: originalRequest });
+            })
+          }
+          // 还没有刷新token，加锁，尝试刷新token
+          isRefreshing = true;
+          try {
+            await options.onUnauthorized?.();
+
+            const queueCopy = [...failedRequestsQueue];
+            failedRequestsQueue = [];
+            queueCopy.forEach(({ resolve, reject, config }) => {
+              service(config).then(resolve).catch(reject);
+            })
+            return service(originalRequest);
+          } catch (err) {
+            failedRequestsQueue.forEach(({ reject }) => reject(err));
+            failedRequestsQueue = [];
+            return Promise.reject(err);
+          } finally {
+            isRefreshing = false;
+          }
         }
         const msg = error.response.data?.message || `网络请求错误 (${error.response.status})`;
         options.onErrorMessage?.(msg);
